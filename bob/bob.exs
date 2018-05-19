4f15703f96a0641e0103e61baf7de06377fd5550
Aaron Milam <aaron@lesson.ly>
2018-05-19 14:27:04 -0400

    Done with bob, I guess? I've been away for a while

--- bob/bob.exs
+++ bob/bob.exs
@@ -1,10 +1,22 @@
 defmodule Bob do
   def hey(input) do
     cond do
-      String.trim(input) == "" -> "Fine. Be that way!"
-      String.ends_with?(input, "?") -> "Sure."
-      String.match?(input, ~r/[[:alpha:]]/) && String.upcase(input) == input -> "Whoa, chill out!"
+      said_nothing?(input) -> "Fine. Be that way!"
+      question?(input) -> "Sure."
+      shouting?(input) -> "Whoa, chill out!"
       true -> "Whatever."
     end
+  end
+
+  def said_nothing?(input) do
+    String.trim(input) == ""
+  end
+
+  def question?(input) do
+    String.ends_with?(input, "?")
+  end
+
+  def shouting?(input) do
+    String.match?(input, ~r/[[:alpha:]]/) && String.upcase(input) == input
   end
 end
